88f074a2349e1d0ffe1377bd4e9f03eb24678151
Jay Fallon <jayfallon@gmail.com>
2019-02-13 14:58:20 -0500

    added authentication via firebase

---
+++ src/components/Landing/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LandingPage = () => (
+	<>
+		<h1>Landing</h1>
+	</>
+);
+
+export default LandingPage;
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PasswordForgetPage = () => (
+	<>
+		<h1>PasswordForget</h1>
+	</>
+);
+
+export default PasswordForgetPage;
