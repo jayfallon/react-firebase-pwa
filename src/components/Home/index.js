ad73eb096f36b9c91e4bbff2017a8669056762c4
Jay Fallon <jayfallon@gmail.com>
2019-02-20 10:26:31 -0500

    added edit and remove to messages

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { compose } from "recompose";
 
-import { withAuthorization, withEmailVerification } from "../Session";
+import { AuthUserContext, withAuthorization, withEmailVerification } from "../Session";
 import { withFirebase } from "../Firebase";
 
 const HomePage = () => (
@@ -51,47 +51,130 @@ class MessagesBase extends Component {
 		this.setState({ text: event.target.value });
 	};
 
-	onCreateMessage = event => {
+	onCreateMessage = (event, authUser) => {
 		this.props.firebase.messages().push({
 			text: this.state.text,
+			userId: authUser.uid,
+			createdAt: this.props.firebase.serverValue.TIMESTAMP,
 		});
-
 		this.setState({ text: "" });
-
 		event.preventDefault();
+	};
+
+	onRemoveMessage = uid => {
+		this.props.firebase.message(uid).remove();
+	};
+
+	onEditMessage = (message, text) => {
+		this.props.firebase.message(message.uid).set({
+			...message,
+			text,
+			editedAt: this.props.firebase.serverValue.TIMESTAMP,
+		});
 	};
 
 	render() {
 		const { text, messages, loading } = this.state;
 
 		return (
-			<div>
-				{loading && <div>Loading...</div>}
+			<AuthUserContext.Consumer>
+				{authUser => (
+					<div>
+						{loading && <div>Loading...</div>}
 
-				{messages ? <MessageList messages={messages} /> : <div>There are no messages...</div>}
+						{messages ? (
+							<MessageList
+								messages={messages}
+								onRemoveMessage={this.onRemoveMessage}
+								onEditMessage={this.onEditMessage}
+							/>
+						) : (
+							<div>There are no messages...</div>
+						)}
 
-				<form onSubmit={this.onCreatMessage}>
-					<input type="text" value={text} onChange={this.onChangeText} />
-					<button type="submit">Send</button>
-				</form>
-			</div>
+						<form onSubmit={event => this.onCreateMessage(event, authUser)}>
+							<input type="text" value={text} onChange={this.onChangeText} />
+							<button type="submit">Send</button>
+						</form>
+					</div>
+				)}
+			</AuthUserContext.Consumer>
 		);
 	}
 }
 
-const MessageList = ({ messages }) => (
+const MessageList = ({ messages, onEditMessage, onRemoveMessage }) => (
 	<ul>
 		{messages.map(message => (
-			<MessageItem key={message.uid} message={message} />
+			<MessageItem
+				key={message.uid}
+				message={message}
+				onEditMessage={onEditMessage}
+				onRemoveMessage={onRemoveMessage}
+			/>
 		))}
 	</ul>
 );
 
-const MessageItem = ({ message }) => (
-	<li>
-		<strong>{message.userId}</strong> {message.text}
-	</li>
-);
+class MessageItem extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			editMode: false,
+			editText: this.props.message.text,
+		};
+	}
+
+	onToggleEditMode = () => {
+		this.setState(state => ({
+			editMode: !state.editMode,
+			editText: this.props.message.text,
+		}));
+	};
+
+	onChangeEditText = event => {
+		this.setState({ editText: event.target.value });
+	};
+
+	onSaveEditText = () => {
+		this.props.onEditMessage(this.props.message, this.state.editText);
+
+		this.setState({ editMode: false });
+	};
+
+	render() {
+		const { message, onRemoveMessage } = this.props;
+		const { editMode, editText } = this.state;
+
+		return (
+			<li>
+				{editMode ? (
+					<input type="text" value={editText} onChange={this.onChangeEditText} />
+				) : (
+					<span>
+						<strong>{message.userId}</strong> {message.text}
+						{message.editedAt && <span>(Edited)</span>}
+					</span>
+				)}
+
+				{editMode ? (
+					<span>
+						<button onClick={this.onSaveEditText}>Save</button>
+						<button onClick={this.onToggleEditMode}>Reset</button>
+					</span>
+				) : (
+					<button onClick={this.onToggleEditMode}>Edit</button>
+				)}
+
+				{!editMode && (
+					<button type="button" onClick={() => onRemoveMessage(message.uid)}>
+						Delete
+					</button>
+				)}
+			</li>
+		);
+	}
+}
 
 const Messages = withFirebase(MessagesBase);
 
